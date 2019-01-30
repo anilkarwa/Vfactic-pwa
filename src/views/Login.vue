a9c7c9bf4ae96126f91fe66150414ed1f5af15e5
nitinagarwal-selotsoft <nitina@selotsoft.com>
2019-01-30 23:09:58 +0530

    login UI done

---
+++ src/views/Login.vue
@@ -0,0 +1,145 @@
+<template>
+<div id="app">
+  <v-app id="inspire">
+    <!-- <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer> -->
+    <v-toolbar color="indigo" dark fixed app>
+      <!-- <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon> -->
+      <v-toolbar-title>VfactIC</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <!-- <v-toolbar-title>Application</v-toolbar-title> -->
+      <v-btn @click="chnageDataBaseModel = true">Change database</v-btn>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex xs12 sm8 md4>
+            <v-card class="elevation-12">
+              <v-toolbar dark color="indigo">
+                <v-toolbar-title >NitinAgarwal</v-toolbar-title>
+                <!-- <v-spacer></v-spacer> -->
+                <!-- <v-tooltip bottom>
+                  <v-btn
+                    icon
+                    large
+                    :href="source"
+                    target="_blank"
+                    slot="activator"
+                  >
+                    <v-icon large>code</v-icon>
+                  </v-btn>
+                  <span>Source</span>
+                </v-tooltip> -->
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
+                  <v-text-field prepend-icon="lock" name="password" label="Password" id="password" type="password"></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="primary">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app inset>
+      <span class="white--text">NitinAgarwal &copy; 2019</span>
+    </v-footer>
+    <!-- START: Code for change database model -->
+    <v-dialog v-model="chnageDataBaseModel" persistent max-width="600px">
+        <v-card>
+          <v-card-title>
+            <span class="headline">User Profile</span>
+          </v-card-title>
+          <v-card-text>
+            <v-container grid-list-md>
+              <v-layout wrap>
+                <v-flex xs12 sm12 md12>
+                  <v-text-field
+                    label="Server *"
+                    hint="example: server ip or domain name"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm12 md12>
+                  <v-text-field
+                    label="DataBase Name *"
+                    hint="example: database which you wanna connect"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm12 md12>
+                  <v-text-field
+                    label="User Name *"
+                    hint="example: username for database connection"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-flex>
+                <v-flex xs12 sm12 md12>
+                  <v-text-field
+                    label="Password *"
+                    hint="example: password for database connection"
+                    persistent-hint
+                    required
+                  ></v-text-field>
+                </v-flex>
+              </v-layout>
+            </v-container>
+            <small>*indicates required field</small>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click="chnageDataBaseModel = false">Close</v-btn>
+            <v-btn color="blue darken-1" flat @click="chnageDataBaseModel = false">Save</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    <!-- END: Code for change database model -->
+  </v-app>
+</div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      chnageDataBaseModel: false
+    }
+  }
+}
+</script>
+<style>
+
+</style>
